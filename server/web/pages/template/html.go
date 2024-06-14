f327369e90ff6c26b8551c510ca8142a0004b760
trinity-aml <trinity1980@fex.net>
2024-06-14 14:09:45 +0300

    update

--- server/web/pages/template/html.go
+++ server/web/pages/template/html.go
@@ -118,20 +118,20 @@ var Mstile150x150png []byte
 //go:embed pages/site.webmanifest
 var Sitewebmanifest []byte
 
-//go:embed pages/static/js/2.15e75e72.chunk.js
-var Staticjs215e75e72chunkjs []byte
+//go:embed pages/static/js/2.fee26a99.chunk.js
+var Staticjs2fee26a99chunkjs []byte
 
-//go:embed pages/static/js/2.15e75e72.chunk.js.LICENSE.txt
-var Staticjs215e75e72chunkjsLICENSEtxt []byte
+//go:embed pages/static/js/2.fee26a99.chunk.js.LICENSE.txt
+var Staticjs2fee26a99chunkjsLICENSEtxt []byte
 
-//go:embed pages/static/js/2.15e75e72.chunk.js.map
-var Staticjs215e75e72chunkjsmap []byte
+//go:embed pages/static/js/2.fee26a99.chunk.js.map
+var Staticjs2fee26a99chunkjsmap []byte
 
-//go:embed pages/static/js/main.60265001.chunk.js
-var Staticjsmain60265001chunkjs []byte
+//go:embed pages/static/js/main.f26b8d1a.chunk.js
+var Staticjsmainf26b8d1achunkjs []byte
 
-//go:embed pages/static/js/main.60265001.chunk.js.map
-var Staticjsmain60265001chunkjsmap []byte
+//go:embed pages/static/js/main.f26b8d1a.chunk.js.map
+var Staticjsmainf26b8d1achunkjsmap []byte
 
 //go:embed pages/static/js/runtime-main.f542387e.js
 var Staticjsruntimemainf542387ejs []byte
